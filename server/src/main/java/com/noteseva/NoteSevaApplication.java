d007f7843a8109deae177babb429e2516a0922cf
goutamoff7 <goutamoff7@gmail.com>
2024-09-26 21:21:32 +0530

    Creating the final year project structure

---
+++ server/src/main/java/com/noteseva/NoteSevaApplication.java
@@ -0,0 +1,13 @@
+package com.noteseva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteSevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteSevaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/noteseva/NoteSevaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noteseva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteSevaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
