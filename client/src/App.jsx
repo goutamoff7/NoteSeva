c1c43c14066d679a69d89e0d37e7a71096505aff
Manojit Das <manojitdas180306@gmail.com>
2024-09-30 12:11:49 +0530

    setup the react all extensions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+// src/App.jsx
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Course from './pages/Course';
+import Project from './pages/Project';
+import Contribute from './pages/Contribute';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
 
-const App = () => {
-  return <div></div>;
-};
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course" element={<Course />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/contribute" element={<Contribute />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
