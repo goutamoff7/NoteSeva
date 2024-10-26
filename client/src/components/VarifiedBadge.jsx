f96dcebeb05223899647f2922aa98c38233478f9
mdramijzamadar <mdramijzamadar@gmail.com>
2024-10-26 18:54:50 +0530

    project section complete

---
+++ client/src/components/VarifiedBadge.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaCheckCircle } from "react-icons/fa";
+const VerifiedBadge = ({ isVerified }) => {
+  return (
+    isVerified && (
+      <div className="flex flex-row items-center gap-1 ">
+        <FaCheckCircle className="text-iconColor " />
+        <span className="text-iconColor text-sm font-normal rounded-full">
+          Verified
+        </span>
+      </div>
+    )
+  );
+};
+
+export default VerifiedBadge;
