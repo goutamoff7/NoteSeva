922fa03a25af514cd7dd76a25bc4c624bb8c8042
goutamoff7 <goutamoff7@gmail.com>
2024-12-02 21:32:28 +0530

    set - current date, username from email id, verification status before saving the file to repository

--- server/src/main/java/com/noteseva/service/OrganizerService.java
+++ server/src/main/java/com/noteseva/service/OrganizerService.java
@@ -1,42 +1,39 @@
 package com.noteseva.service;
 
 import com.noteseva.model.Organizer;
+import com.noteseva.model.VerificationStatus;
 import com.noteseva.repository.OrganizerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
-public class OrganizerService
-{
+public class OrganizerService {
     @Autowired
     OrganizerRepository organizerRepository;
 
-    public List<Organizer> getAllOrganizer()
-    {
+    public List<Organizer> getAllOrganizer() {
         return organizerRepository.findAll();
     }
 
-    public Organizer getOrganizer(Integer id)
-    {
+    public Organizer getOrganizer(Integer id) {
         return organizerRepository.findById(id).orElse(null);
     }
 
-    public Organizer uploadOrganizer(Organizer organizer)
-    {
-        return organizerRepository.save(organizer);
-    }
-
-    public Organizer uploadOrganizer(Organizer organizer, MultipartFile file){
+    public Organizer uploadOrganizer(Organizer organizer, MultipartFile file, String username) {
         try {
             organizer.setFileName(file.getOriginalFilename());
             organizer.setFileType(file.getContentType());
             organizer.setFileData(file.getBytes());
+            organizer.setSharedBy(username);
+            organizer.setDate(LocalDate.now());
+            organizer.setVerificationStatus(VerificationStatus.NOT_VERIFIED);
         } catch (Exception e) {
-            System.out.println(e.getMessage()+"from service class.");
+            System.out.println(e.getMessage() + "from service class.");
         }
         return organizerRepository.save(organizer);
     }
