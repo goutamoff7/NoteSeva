a15600bf37dfbfa01900d7860f70acfb554c2195
goutamoff7 <goutamoff7@gmail.com>
2024-12-02 21:18:47 +0530

    file validation added, set username of the uploader from email id

--- server/src/main/java/com/noteseva/controller/OrganizerController.java
+++ server/src/main/java/com/noteseva/controller/OrganizerController.java
@@ -1,83 +1,91 @@
 package com.noteseva.controller;
-
 import com.noteseva.model.Organizer;
 import com.noteseva.service.OrganizerService;
+import com.noteseva.service.UtilityService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
 @CrossOrigin
 @RestController
 @RequestMapping("organizer")
-public class OrganizerController
-{
+public class OrganizerController {
 
     @Autowired
     OrganizerService organizerService;
 
-    //localhost:8080/organizer
+    @Autowired
+    UtilityService utilityService;
+
+    //localhost:8080/organizer/all
     @GetMapping("/all")
-    public ResponseEntity<?> getAllOrganizer()
-    {
+    public ResponseEntity<?> getAllOrganizer() {
         try {
             return new ResponseEntity<>(organizerService.getAllOrganizer(), HttpStatus.OK);
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            return new ResponseEntity<>("May be Organizers are not available!!",HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("May be Organizers are not available!!", HttpStatus.NOT_FOUND);
         }
     }
 
     //localhost:8080/organizer/1
     @GetMapping("/{id}")
-    public ResponseEntity<?> getOrganizer(@PathVariable Integer id)
-    {
-        try{
+    public ResponseEntity<?> getOrganizer(@PathVariable Integer id) {
+        try {
             Organizer organizer = organizerService.getOrganizer(id);
-            if (organizer!=null)
-                return new ResponseEntity<>(organizer,HttpStatus.OK);
+            if (organizer != null)
+                return new ResponseEntity<>(organizer, HttpStatus.OK);
             else
-                return new ResponseEntity<>("May be this organizer is not available!!",HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>("May be this organizer is not available!!", HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    //localhost:8080/uploadOrganizer
+    //localhost:8080/organizer/upload
     @PostMapping("/upload")
-    public ResponseEntity<?> uploadOrganizer(@RequestPart Organizer organizer,
-                                             @RequestPart MultipartFile file)
-    {
-        try{
-            Organizer organizer1 = organizerService.uploadOrganizer(organizer,file);
-            return  new ResponseEntity<>(organizer1,HttpStatus.CREATED);
+    public ResponseEntity<?> uploadOrganizer(@RequestPart @Valid Organizer organizer,
+                                             @RequestPart MultipartFile file) {
+        try {
+            // Validate the file
+            utilityService.validateFile(file);
+
+            //Getting uploader name
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+            // Process and save organizer and file
+            Organizer organizer1 = organizerService.uploadOrganizer(organizer, file, username);
+            return new ResponseEntity<>(organizer1, HttpStatus.CREATED);
+
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            return  new ResponseEntity<>("Something Went Wrong!!",HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("Something went wrong!!", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    //localhost:8080/downloadOrganizer/1
+    //localhost:8080/organizer/download/1
     @GetMapping("/download/{id}")
-    public ResponseEntity<?> downloadOrganizer(@PathVariable Integer id)
-    {
-        try{
+    public ResponseEntity<?> downloadOrganizer(@PathVariable Integer id) {
+        try {
             Organizer organizer = organizerService.getOrganizer(id);
-            if (organizer!=null)
-            {
+            if (organizer != null) {
                 byte[] fileData = organizer.getFileData();
                 String fileType = organizer.getFileType();
                 HttpHeaders headers = new HttpHeaders();
                 headers.setContentType(MediaType.valueOf(fileType));
                 return new ResponseEntity<>(fileData, headers, HttpStatus.OK);
-            }
-            else
-                return new ResponseEntity<>("May be this organizer is not available!!",HttpStatus.NOT_FOUND);
+            } else
+                return new ResponseEntity<>("May be this organizer is not available!!", HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
