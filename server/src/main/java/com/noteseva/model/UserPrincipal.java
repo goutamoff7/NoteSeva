329a2fa49f86b797554ecf82a37926612af99af8
goutamoff7 <goutamoff7@gmail.com>
2024-12-02 21:25:18 +0530

    removed extra space

--- server/src/main/java/com/noteseva/model/UserPrincipal.java
+++ server/src/main/java/com/noteseva/model/UserPrincipal.java
@@ -1,10 +1,7 @@
 package com.noteseva.model;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.List;
 
