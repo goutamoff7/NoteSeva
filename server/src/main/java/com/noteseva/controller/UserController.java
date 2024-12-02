6d4b912289cb1591979d28a4601436ba749cf76e
goutamoff7 <goutamoff7@gmail.com>
2024-12-02 21:15:41 +0530

    validation added in register API - if user exist

--- server/src/main/java/com/noteseva/controller/UserController.java
+++ server/src/main/java/com/noteseva/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.noteseva.controller;
 
 import com.noteseva.model.Users;
+import com.noteseva.repository.UserRepository;
 import com.noteseva.service.UserService;
+import com.noteseva.service.UtilityService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,22 +22,34 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UtilityService utilityService;
+
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody @Valid Users user , BindingResult bindingResult)
-    {
-        if(bindingResult.hasErrors()){
-            Map<String , String> errors=new HashMap<>();
-            bindingResult.getFieldErrors().forEach(error->
-                    errors.put(error.getField(),error.getDefaultMessage()));
-            return  new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> register(@RequestBody @Valid Users user) {
+        try {
+            String username = utilityService.extractUsernameFromEmail(user);
+            if (userRepository.findByUsername(username)==null)
+                return new ResponseEntity<>(userService.register(user), HttpStatus.CREATED);
+            return new ResponseEntity<>("User already exist",HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>("User Registration Unsuccessful", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<>(userService.register(user), HttpStatus.CREATED);
+
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody Users user)
-    {
-        return new ResponseEntity<>(userService.verify(user),HttpStatus.OK);
+    public ResponseEntity<?> login(@RequestBody @Valid Users user) {
+        try {
+            return new ResponseEntity<>(userService.verify(user), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Login Failed", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
 }
