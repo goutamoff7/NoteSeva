6a9c04d33561d9da072a9e9f2900948aba21e840
Kundu-A <kunduarpan43@gmail.com>
2025-03-18 17:43:31 +0530

    Add a method to get Users object by email

--- server/src/main/java/com/noteseva/repository/UserRepository.java
+++ server/src/main/java/com/noteseva/repository/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<Users,Integer> {
 
     Users findByUsername(String username);
+
+    Users findUsersByEmail(String email);
 }
