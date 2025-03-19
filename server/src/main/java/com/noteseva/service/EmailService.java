1a457a76a4c6483325e279f5bf389d5aa247d2b4
goutamoff7 <goutamoff7@gmail.com>
2025-03-19 09:17:46 +0530

    refractor:sending plain String to html

--- server/src/main/java/com/noteseva/service/EmailService.java
+++ server/src/main/java/com/noteseva/service/EmailService.java
@@ -4,7 +4,6 @@ import com.noteseva.exception.EmailSendingException;
 import com.noteseva.repository.UserRepository;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -25,29 +24,38 @@ public class EmailService {
     private static final String fromMail = "noteseva1308@gmail.com";
 
     @Async
-    public void sendOTP(String email){
+    public void sendOTP(String email) {
 
         try {
             MimeMessage message = javaMailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
             String otp = otpService.generateOTP(email);
-            String name=userRepo.findUsersByEmail(email).getName();
-
-            String htmlContent = "<div style='font-family: Arial, sans-serif; padding: 15px; border: 1px solid #ddd; border-radius: 10px;'>"
-                    + "<h2 style='color: #2c3e50;'>One-Time Password (OTP) Verification</h2>"
-                    + "<p style='font-size: 16px;'>Dear "+name+",</p>"
-                    + "<p style='font-size: 16px;'>Your OTP for email verification is: "
-                    + "<strong style='color: #e74c3c; font-size: 20px;'>" + otp + "</strong></p>"
-                    + "<p style='font-size: 14px; color: #7f8c8d;'>This OTP is valid for 1 minutes. Do not share it with anyone.</p>"
-                    + "<p style='margin-top: 20px;'>Regards,<br><strong>NOTESEVA Team</strong></p>"
-                    + "</div>";
 
             helper.setTo(email);
             helper.setSubject("One-Time Password (OTP) Verification");
-            helper.setText(htmlContent, true);
+            helper.setText(getEmailBody(Integer.parseInt(otp)), true);
             helper.setFrom(fromMail);
 
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new EmailSendingException("Failed to send OTP");
+        }
+    }
+
+    @Async
+    public void sendEmail(String email) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            String name = userRepo.findUsersByEmail(email).getName();
+
+            helper.setTo(email);
+            helper.setSubject("Registration Successful - NoteSeva");
+            helper.setText(getEmailBody(name),true);
+            helper.setFrom(fromMail);
             javaMailSender.send(message);
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -55,25 +63,8 @@ public class EmailService {
         }
     }
 
-    @Async
-    public void sendEmail(String mailTo) {
-        try {
-            String name=userRepo.findUsersByEmail(mailTo).getName();
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(mailTo);
-            message.setSubject(getEmailSubject());
-            message.setText(getEmailBody(name));
-            message.setFrom(fromMail);
-            javaMailSender.send(message);
-        }catch(Exception e) {
-            System.out.println(e.getMessage());
-            throw new EmailSendingException("Failed to send OTP email");
-        }
-    }
-
-    public String getEmailBody(String name)
-    {
-        return  "<div style='font-family: Arial, sans-serif; padding: 15px; border: 1px solid #ddd; border-radius: 10px;'>"
+    public String getEmailBody(String name) {
+        return "<div style='font-family: Arial, sans-serif; padding: 15px; border: 1px solid #ddd; border-radius: 10px;'>"
                 + "<h2 style='color: #2c3e50;'>Welcome to NoteSeva!</h2>"
                 + "<p style='font-size: 16px;'>Dear " + name + ",</p>"
                 + "<p style='font-size: 16px;'>Your registration with <strong>NoteSeva</strong> has been successfully completed!</p>"
@@ -84,8 +75,15 @@ public class EmailService {
 
     }
 
-    public String getEmailSubject()
-    {
-        return "Registration Successful - NoteSeva";
+    public String getEmailBody(Integer otp) {
+        return "<div style='font-family: Arial, sans-serif; padding: 15px; border: 1px solid #ddd; border-radius: 10px;'>"
+                + "<h2 style='color: #2c3e50;'>One-Time Password (OTP) Verification</h2>"
+                + "<p style='font-size: 16px;'>Dear User,</p>"
+                + "<p style='font-size: 16px;'>Your OTP for email verification is: "
+                + "<strong style='color: #e74c3c; font-size: 20px;'>" + otp + "</strong></p>"
+                + "<p style='font-size: 14px; color: #7f8c8d;'>This OTP is valid for 10 minutes. Do not share it with anyone.</p>"
+                + "<p style='margin-top: 20px;'>Regards,<br><strong>NOTESEVA Team</strong></p>"
+                + "</div>";
+
     }
 }
