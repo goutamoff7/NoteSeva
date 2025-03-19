29b557b1d0ef811069ec940044a5e0e4ee343970
goutamoff7 <goutamoff7@gmail.com>
2025-03-19 09:24:08 +0530

    renamed UserService to PublicService

--- server/src/main/java/com/noteseva/service/PublicService.java
+++ server/src/main/java/com/noteseva/service/PublicService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
-public class UserService {
+public class PublicService {
 
     @Autowired
     PasswordEncoder passwordEncoder;
