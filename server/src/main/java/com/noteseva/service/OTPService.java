f6f7abd463775aba972c34b8819ee5c8bf910274
goutamoff7 <goutamoff7@gmail.com>
2025-03-23 10:47:42 +0530

    generate and validate otp

---
+++ server/src/main/java/com/noteseva/service/OTPService.java
@@ -0,0 +1,28 @@
+package com.noteseva.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class OTPService {
+    @Autowired
+    private RedisService redisService;
+    // 6 Digit otp
+    public String generateOTP(String email) {
+        String otp = String.valueOf(new Random().nextInt(900000) + 100000);
+        redisService.saveOTP(email, otp);
+        return otp;
+    }
+
+    public boolean validateOTP(String email, String userOtp) {
+        String storedOtp = redisService.getOTP(email);
+        if (storedOtp != null && storedOtp.equals(userOtp)) {
+            redisService.deleteOTP(email);
+            redisService.markEmailAsVerified(email);
+            return true;
+        }
+        return false;
+    }
+}
