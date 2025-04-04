fa2b94ae1456d1c0a1990f8c6cd5497459dbce5b
goutamoff7 <goutamoff7@gmail.com>
2025-04-04 23:50:50 +0530

    Geenerating cookie to save access and refresh token

--- server/src/main/java/com/noteseva/service/PublicService.java
+++ server/src/main/java/com/noteseva/service/PublicService.java
@@ -1,19 +1,19 @@
 package com.noteseva.service;
 
-import com.noteseva.DTO.PasswordDTO;
 import com.noteseva.model.Role;
+import com.noteseva.model.TokenExpiration;
 import com.noteseva.model.Users;
 import com.noteseva.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.time.Duration;
 import java.util.UUID;
 
 @Service
@@ -34,8 +34,11 @@ public class PublicService {
     @Autowired
     UtilityService utilityService;
 
+    @Autowired
+    TokenExpiration tokenExpiration;
+
+
     public Users register(Users user) {
-        user.setUsername(utilityService.extractUsernameFromEmail(user.getEmail())); // username = substring of email id, before @ symbol
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole(Role.ROLE_USER);
         return userRepository.save(user);
@@ -51,23 +54,65 @@ public class PublicService {
         return userRepository.save(user);
     }
 
-    public String verify(Users user) {
-        String username=utilityService.extractUsernameFromEmail(user.getEmail());
+    public boolean verify(Users user) {
         Authentication authentication =
                 authenticationManager.authenticate(
                         new UsernamePasswordAuthenticationToken(
-                                username,
+                                user.getUsername(),
                                 user.getPassword()
                         )
                 );
-        if (authentication.isAuthenticated())
-            return jwtService.generateToken(username);
-        return "Login Failed";
+        return authentication.isAuthenticated();
+
     }
 
+    public Users saveRefreshToken(String username, String refreshToken){
+        Users user = findByUsername(username);
+        user.setRefreshToken(passwordEncoder.encode(refreshToken));
+        return userRepository.save(user);
+    }
 
     public Users resetPassword(Users user, String newPassword) {
         user.setPassword(passwordEncoder.encode(newPassword));
         return userRepository.save(user);
     }
+
+    public ResponseCookie getAccessTokenCookie(String token){
+       return  ResponseCookie
+                .from("access-token", token)
+                .httpOnly(true)
+                .secure(false)
+                //(false) Allows the cookie to be sent over both HTTP and HTTPS.
+                //Ensures cookies are sent only over HTTPS (recommended for production).
+                .path("/")
+                .maxAge(Duration.ofSeconds(tokenExpiration.getAccessTokenExpiration()))
+                .build();
+    }
+
+    public ResponseCookie getRefreshTokenCookie(String token){
+        return  ResponseCookie
+                .from("refresh-token", token)
+                .httpOnly(true)
+                .secure(false)
+                //(false) Allows the cookie to be sent over both HTTP and HTTPS.
+                //Ensures cookies are sent only over HTTPS (recommended for production).
+                .path("/")
+                .maxAge(Duration.ofSeconds(tokenExpiration.getRefreshTokenExpiration()))
+                .build();
+    }
+
+    public Users findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+
+    public boolean validateRefreshToken(String refreshToken,String usernameClaimed){
+        Users user = findByUsername(usernameClaimed);
+        String savedRefreshToken = user.getRefreshToken();
+        return isTokenMatch(refreshToken,savedRefreshToken);
+    }
+
+    public boolean isTokenMatch(String refreshToken, String savedRefreshToken){
+        return passwordEncoder.matches(refreshToken,savedRefreshToken);
+    }
 }
+
