22fb9d9f825fbcbfcdab20dda56324a1390f95d5
mdramijzamadar <mdramijzamadar@gmail.com>
2025-04-11 10:38:04 +0530

    add features

--- client/src/components/ProfileCard.jsx
+++ client/src/components/ProfileCard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const ProfileCard = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/my-profile");
+  };
+
   return (
     <div className="flex flex-col items-center justify-between bg-[#1E293B] border border-gray-100 text-white p-4 rounded-2xl gap-4">
       <div className="w-16 h-16 rounded-md shadow-lg overflow-hidden">
@@ -16,7 +23,10 @@ const ProfileCard = () => {
         <FaInstagram />
         <FaLinkedin />
       </div>
-      <button className="bg-[#263238] border border-[#F0F6FC1A] px-6 py-1">
+      <button
+        onClick={handleClick}
+        className="bg-[#263238] border border-[#F0F6FC1A] px-6 py-1"
+      >
         Edit Profile
       </button>
     </div>
