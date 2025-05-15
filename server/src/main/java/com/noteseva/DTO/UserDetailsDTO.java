fdae2763801cefcd46661f7f3bd27b0295658c54
goutamoff7 <goutamoff7@gmail.com>
2025-05-15 18:52:30 +0530

    UserDetailsDTO added to provide User details in response

---
+++ server/src/main/java/com/noteseva/DTO/UserDetailsDTO.java
@@ -0,0 +1,52 @@
+package com.noteseva.DTO;
+
+import com.noteseva.constants.Role;
+import com.noteseva.model.Notes;
+import com.noteseva.model.Organizer;
+import com.noteseva.model.PYQ;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Setter
+public class UserDetailsDTO {
+
+    private Integer id;
+
+    private String username;
+
+    private Role role;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime lastLoginAt;
+
+    private String gender;
+
+    private String collegeName;
+
+    private String imageUrl;
+
+    private String linkedInUrl;
+
+    private String gitHubUrl;
+
+    private String otherUrl;
+
+    private String refreshToken;
+
+    private Set<Notes> notes;
+
+    private Set<Organizer> organizer;
+
+    private Set<PYQ> pyq;
+}
